5f2c3c83eb23ab7f491b5fc9f01a2a41a1e2a996
TomarJatin <jt24072002@gmail.com>
2023-11-18 00:39:41 +0530

    buttons added

---
+++ GlobalStyle.ts
@@ -0,0 +1,16 @@
+export const Color = {
+    backgroundColor: "#181818",
+    textColor: "#fff",
+    card: "#1F3664",
+    greenText: "#25FF6F",
+    redText: "#FF4D4D"
+}
+
+export const FontSize = {
+    xxs: 10,
+    xs: 12,
+    xs13: 13,
+    sm: 14,
+    md: 16,
+    lg: 20
+}
