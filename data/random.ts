147a05a2ae4673330dbc2bc0b5f1fc6f17f2ee75
TomarJatin <jt24072002@gmail.com>
2023-11-26 02:54:03 +0530

    quiz ui done

--- data/random.ts
+++ data/random.ts
@@ -1,182 +1,209 @@
-export const quiz_questions = [
-    {
-        "quiz_img": "url1",
-        "question": "Who is the main protagonist of Naruto?",
-        "options": ["A. Sasuke", "B. Naruto", "C. Sakura"],
-        "answer": "B",
-        "anime": "Naruto",
-        "tag": "Character",
-        "difficulty": 60
-    },
-    {
-        "quiz_img": "url2",
-        "question": "In 'Death Note,' what is the main character's alias?",
-        "options": ["A. L", "B. Light Yagami", "C. Misa Amane"],
-        "answer": "B",
-        "anime": "Death Note",
-        "tag": "Character",
-        "difficulty": 75
-    },
-    {
-        "quiz_img": "url3",
-        "question": "Which anime features a group of pirates searching for the One Piece?",
-        "options": ["A. Dragon Ball", "B. One Piece", "C. Attack on Titan"],
-        "answer": "B",
-        "anime": "One Piece",
-        "tag": "Series",
-        "difficulty": 50
-    },
-    {
-        "quiz_img": "url4",
-        "question": "What is the name of the giant humanoid creatures in 'Attack on Titan'?",
-        "options": ["A. Titans", "B. Giants", "C. Colossals"],
-        "answer": "A",
-        "anime": "Attack on Titan",
-        "tag": "Creature",
-        "difficulty": 70
-    },
-    {
-        "quiz_img": "url5",
-        "question": "In 'My Hero Academia,' what is the main character's Quirk?",
-        "options": ["A. Explosion", "B. One For All", "C. Half-Cold Half-Hot"],
-        "answer": "B",
-        "anime": "My Hero Academia",
-        "tag": "Quirk",
-        "difficulty": 65
-    },
-    {
-        "quiz_img": "url6",
-        "question": "Which anime features a high school club dedicated to finding supernatural phenomena?",
-        "options": ["A. Hyouka", "B. Steins;Gate", "C. The Melancholy of Haruhi Suzumiya"],
-        "answer": "C",
-        "anime": "The Melancholy of Haruhi Suzumiya",
-        "tag": "Series",
-        "difficulty": 55
-    },
-    {
-        "quiz_img": "url7",
-        "question": "What is the signature move of Goku in 'Dragon Ball Z'?",
-        "options": ["A. Kamehameha", "B. Spirit Bomb", "C. Final Flash"],
-        "answer": "A",
-        "anime": "Dragon Ball Z",
-        "tag": "Technique",
-        "difficulty": 80
-    },
-    {
-        "quiz_img": "url8",
-        "question": "In 'Sword Art Online,' what is the name of the virtual reality MMORPG?",
-        "options": ["A. Aincrad", "B. Alfheim", "C. GGO"],
-        "answer": "A",
-        "anime": "Sword Art Online",
-        "tag": "Game",
-        "difficulty": 70
-    },
-    {
-        "quiz_img": "url9",
-        "question": "Who is the main character in 'Fullmetal Alchemist'?",
-        "options": ["A. Edward Elric", "B. Alphonse Elric", "C. Roy Mustang"],
-        "answer": "A",
-        "anime": "Fullmetal Alchemist",
-        "tag": "Character",
-        "difficulty": 65
-    },
-    {
-        "quiz_img": "url10",
-        "question": "What is the name of the ghost girl in 'Bleach' who guides souls to the afterlife?",
-        "options": ["A. Orihime", "B. Rukia", "C. Yoruichi"],
-        "answer": "B",
-        "anime": "Bleach",
-        "tag": "Character",
-        "difficulty": 60
-    },
-    {
-        "quiz_img": "url11",
-        "question": "In 'Cowboy Bebop,' what is the name of the spaceship the main characters travel on?",
-        "options": ["A. Swordfish II", "B. Bebop", "C. Red Tail"],
-        "answer": "B",
-        "anime": "Cowboy Bebop",
-        "tag": "Vehicle",
-        "difficulty": 75
-    },
-    {
-        "quiz_img": "url12",
-        "question": "What is the power of Natsu Dragneel in 'Fairy Tail'?",
-        "options": ["A. Ice Magic", "B. Fire Dragon Slayer Magic", "C. Telekinesis"],
-        "answer": "B",
-        "anime": "Fairy Tail",
-        "tag": "Magic",
-        "difficulty": 70
-    },
-    {
-        "quiz_img": "url13",
-        "question": "Who is the creator of 'Neon Genesis Evangelion'?",
-        "options": ["A. Hayao Miyazaki", "B. Hideaki Anno", "C. Makoto Shinkai"],
-        "answer": "B",
-        "anime": "Neon Genesis Evangelion",
-        "tag": "Creator",
-        "difficulty": 55
-    },
-    {
-        "quiz_img": "url14",
-        "question": "What is the name of the demon fox sealed within Naruto?",
-        "options": ["A. Kurama", "B. Shukaku", "C. Matatabi"],
-        "answer": "A",
-        "anime": "Naruto",
-        "tag": "Creature",
-        "difficulty": 65
-    },
-    {
-        "quiz_img": "url15",
-        "question": "In 'One Punch Man,' what is Saitama's superhero name?",
-        "options": ["A. Genos", "B. Mumen Rider", "C. One Punch Man"],
-        "answer": "C",
-        "anime": "One Punch Man",
-        "tag": "Character",
-        "difficulty": 60
-    },
-    {
-        "quiz_img": "url16",
-        "question": "Which anime is set in the world of alchemy and follows the Elric brothers?",
-        "options": ["A. Attack on Titan", "B. Fullmetal Alchemist", "C. My Hero Academia"],
-        "answer": "B",
-        "anime": "Fullmetal Alchemist",
-        "tag": "Series",
-        "difficulty": 65
-    },
-    {
-        "quiz_img": "url17",
-        "question": "What is the name of the robotic cat in 'Doraemon'?",
-        "options": ["A. Nobita", "B. Shizuka", "C. Doraemon"],
-        "answer": "C",
-        "anime": "Doraemon",
-        "tag": "Character",
-        "difficulty": 50
-    },
-    {
-        "quiz_img": "url18",
-        "question": "In 'Demon Slayer,' what is the main character Tanjiro Kamado's weapon?",
-        "options": ["A. Katana", "B. Bow and Arrow", "C. Dual Swords"],
-        "answer": "A",
-        "anime": "Demon Slayer",
-        "tag": "Weapon",
-        "difficulty": 70
-    },
-    {
-        "quiz_img": "url19",
-        "question": "Who is known as the 'Strongest Hero' in 'One Punch Man'?",
-        "options": ["A. Genos", "B. Saitama", "C. Mumen Rider"],
-        "answer": "B",
-        "anime": "One Punch Man",
-        "tag": "Title",
-        "difficulty": 75
-    },
-    {
-        "quiz_img": "url20",
-        "question": "What is the name of the magical school in 'Little Witch Academia'?",
-        "options": ["A. Hogwarts", "B. Luna Nova", "C. WizTech"],
-        "answer": "B",
-        "anime": "Little Witch Academia",
-        "tag": "Location",
-        "difficulty": 55
-    }
-]
+interface QuizQuestionObj{
+    quiz_img: string,
+    question: string,
+    options: string[],
+    answer: string,
+    anime: string,
+    tag: string,
+    difficulty: number,
+}
+
+export const quiz_questions: QuizQuestionObj[] = [
+  {
+    quiz_img: "url1",
+    question: "Who is the main protagonist of Naruto?",
+    options: ["A. Sasuke", "B. Naruto", "C. Sakura"],
+    answer: "B",
+    anime: "Naruto",
+    tag: "Character",
+    difficulty: 60,
+  },
+  {
+    quiz_img: "url2",
+    question: "In 'Death Note,' what is the main character's alias?",
+    options: ["A. L", "B. Light Yagami", "C. Misa Amane"],
+    answer: "B",
+    anime: "Death Note",
+    tag: "Character",
+    difficulty: 75,
+  },
+  {
+    quiz_img: "url3",
+    question:
+      "Which anime features a group of pirates searching for the One Piece?",
+    options: ["A. Dragon Ball", "B. One Piece", "C. Attack on Titan"],
+    answer: "B",
+    anime: "One Piece",
+    tag: "Series",
+    difficulty: 50,
+  },
+  {
+    quiz_img: "url4",
+    question:
+      "What is the name of the giant humanoid creatures in 'Attack on Titan'?",
+    options: ["A. Titans", "B. Giants", "C. Colossals"],
+    answer: "A",
+    anime: "Attack on Titan",
+    tag: "Creature",
+    difficulty: 70,
+  },
+  {
+    quiz_img: "url5",
+    question: "In 'My Hero Academia,' what is the main character's Quirk?",
+    options: ["A. Explosion", "B. One For All", "C. Half-Cold Half-Hot"],
+    answer: "B",
+    anime: "My Hero Academia",
+    tag: "Quirk",
+    difficulty: 65,
+  },
+  {
+    quiz_img: "url6",
+    question:
+      "Which anime features a high school club dedicated to finding supernatural phenomena?",
+    options: [
+      "A. Hyouka",
+      "B. Steins;Gate",
+      "C. The Melancholy of Haruhi Suzumiya",
+    ],
+    answer: "C",
+    anime: "The Melancholy of Haruhi Suzumiya",
+    tag: "Series",
+    difficulty: 55,
+  },
+  {
+    quiz_img: "url7",
+    question: "What is the signature move of Goku in 'Dragon Ball Z'?",
+    options: ["A. Kamehameha", "B. Spirit Bomb", "C. Final Flash"],
+    answer: "A",
+    anime: "Dragon Ball Z",
+    tag: "Technique",
+    difficulty: 80,
+  },
+  {
+    quiz_img: "url8",
+    question:
+      "In 'Sword Art Online,' what is the name of the virtual reality MMORPG?",
+    options: ["A. Aincrad", "B. Alfheim", "C. GGO"],
+    answer: "A",
+    anime: "Sword Art Online",
+    tag: "Game",
+    difficulty: 70,
+  },
+  {
+    quiz_img: "url9",
+    question: "Who is the main character in 'Fullmetal Alchemist'?",
+    options: ["A. Edward Elric", "B. Alphonse Elric", "C. Roy Mustang"],
+    answer: "A",
+    anime: "Fullmetal Alchemist",
+    tag: "Character",
+    difficulty: 65,
+  },
+  {
+    quiz_img: "url10",
+    question:
+      "What is the name of the ghost girl in 'Bleach' who guides souls to the afterlife?",
+    options: ["A. Orihime", "B. Rukia", "C. Yoruichi"],
+    answer: "B",
+    anime: "Bleach",
+    tag: "Character",
+    difficulty: 60,
+  },
+  {
+    quiz_img: "url11",
+    question:
+      "In 'Cowboy Bebop,' what is the name of the spaceship the main characters travel on?",
+    options: ["A. Swordfish II", "B. Bebop", "C. Red Tail"],
+    answer: "B",
+    anime: "Cowboy Bebop",
+    tag: "Vehicle",
+    difficulty: 75,
+  },
+  {
+    quiz_img: "url12",
+    question: "What is the power of Natsu Dragneel in 'Fairy Tail'?",
+    options: ["A. Ice Magic", "B. Fire Dragon Slayer Magic", "C. Telekinesis"],
+    answer: "B",
+    anime: "Fairy Tail",
+    tag: "Magic",
+    difficulty: 70,
+  },
+  {
+    quiz_img: "url13",
+    question: "Who is the creator of 'Neon Genesis Evangelion'?",
+    options: ["A. Hayao Miyazaki", "B. Hideaki Anno", "C. Makoto Shinkai"],
+    answer: "B",
+    anime: "Neon Genesis Evangelion",
+    tag: "Creator",
+    difficulty: 55,
+  },
+  {
+    quiz_img: "url14",
+    question: "What is the name of the demon fox sealed within Naruto?",
+    options: ["A. Kurama", "B. Shukaku", "C. Matatabi"],
+    answer: "A",
+    anime: "Naruto",
+    tag: "Creature",
+    difficulty: 65,
+  },
+  {
+    quiz_img: "url15",
+    question: "In 'One Punch Man,' what is Saitama's superhero name?",
+    options: ["A. Genos", "B. Mumen Rider", "C. One Punch Man"],
+    answer: "C",
+    anime: "One Punch Man",
+    tag: "Character",
+    difficulty: 60,
+  },
+  {
+    quiz_img: "url16",
+    question:
+      "Which anime is set in the world of alchemy and follows the Elric brothers?",
+    options: [
+      "A. Attack on Titan",
+      "B. Fullmetal Alchemist",
+      "C. My Hero Academia",
+    ],
+    answer: "B",
+    anime: "Fullmetal Alchemist",
+    tag: "Series",
+    difficulty: 65,
+  },
+  {
+    quiz_img: "url17",
+    question: "What is the name of the robotic cat in 'Doraemon'?",
+    options: ["A. Nobita", "B. Shizuka", "C. Doraemon"],
+    answer: "C",
+    anime: "Doraemon",
+    tag: "Character",
+    difficulty: 50,
+  },
+  {
+    quiz_img: "url18",
+    question:
+      "In 'Demon Slayer,' what is the main character Tanjiro Kamado's weapon?",
+    options: ["A. Katana", "B. Bow and Arrow", "C. Dual Swords"],
+    answer: "A",
+    anime: "Demon Slayer",
+    tag: "Weapon",
+    difficulty: 70,
+  },
+  {
+    quiz_img: "url19",
+    question: "Who is known as the 'Strongest Hero' in 'One Punch Man'?",
+    options: ["A. Genos", "B. Saitama", "C. Mumen Rider"],
+    answer: "B",
+    anime: "One Punch Man",
+    tag: "Title",
+    difficulty: 75,
+  },
+  {
+    quiz_img: "url20",
+    question:
+      "What is the name of the magical school in 'Little Witch Academia'?",
+    options: ["A. Hogwarts", "B. Luna Nova", "C. WizTech"],
+    answer: "B",
+    anime: "Little Witch Academia",
+    tag: "Location",
+    difficulty: 55,
+  },
+];
--- screens/Quiz.tsx
+++ screens/Quiz.tsx
@@ -11,107 +11,195 @@ import {
 import { SafeAreaView } from "react-native-safe-area-context";
 import { Color, FontSize } from "../GlobalStyle";
 import { Ionicons } from "@expo/vector-icons";
-import { Feather } from '@expo/vector-icons';
+import { Feather } from "@expo/vector-icons";
 import GeneralButton from "../components/Button";
 import { Image } from "expo-image";
-import { useContext, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import { DataContext } from "../contexts/DataContext";
+import { quiz_questions } from "../data/random";
+
+interface QuizQuestionObj {
+  quiz_img: string;
+  question: string;
+  options: string[];
+  answer: string;
+  anime: string;
+  tag: string;
+  difficulty: number;
+}
 
 export default function Quiz({ navigation }: any) {
   const { selectLvl } = useContext(DataContext);
   const [loading, setLoading] = useState(false);
-  const [quizQuestions, setQuizQuestions] = useState([]);
+  const [quizQuestions, setQuizQuestions] = useState<QuizQuestionObj[]>([]);
+  const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
   const [quizAnswers, setQuizAnswers] = useState([]);
 
   const createQuiz = () => {
     setLoading(true);
-  }
+    const _quiz_questions = quiz_questions
+      .sort((a, b) => {
+        return a.difficulty - b.difficulty;
+      })
+      .slice(0, 10);
+    setQuizQuestions([..._quiz_questions]);
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    createQuiz();
+  }, []);
 
   return (
     <SafeAreaView>
-      <FlatList
-        data={["1"]}
-        renderItem={() => (
-          <>
-            {/* Topbar */}
-            <View
-              style={{
-                width: "100%",
-                flexDirection: "row",
-                justifyContent: "space-between",
-                paddingHorizontal: 15,
-                paddingVertical: 10,
-                alignItems: "center",
-              }}
-            >
+      {!loading ? (
+        <FlatList
+          data={["1"]}
+          renderItem={() => (
+            <>
+              {/* Topbar */}
               <View
                 style={{
+                  width: "100%",
                   flexDirection: "row",
-                  gap: 20,
+                  justifyContent: "space-between",
+                  paddingHorizontal: 15,
+                  paddingVertical: 10,
                   alignItems: "center",
                 }}
               >
-                <GeneralButton
-                  title="Back"
-                  width={60}
-                  paddingVertical={10}
-                  onPress={() => {
-                    navigation.goBack();
+                <View
+                  style={{
+                    flexDirection: "row",
+                    gap: 20,
+                    alignItems: "center",
                   }}
-                  borderRadius={8}
-                />
+                >
+                  <GeneralButton
+                    title="Back"
+                    width={60}
+                    paddingVertical={10}
+                    onPress={() => {
+                      navigation.goBack();
+                    }}
+                    borderRadius={8}
+                  />
+                  <Text
+                    style={{
+                      color: Color.textColor,
+                      fontSize: FontSize.sm,
+                      fontWeight: "700",
+                    }}
+                  >
+                    Select Level
+                  </Text>
+                </View>
+                <View
+                  style={{
+                    flexDirection: "row",
+                    gap: 20,
+                    alignItems: "center",
+                  }}
+                >
+                  <Feather name="bookmark" size={24} color="white" />
+                  <Ionicons name="settings-sharp" size={30} color="white" />
+                </View>
+              </View>
+
+              <View
+                style={{
+                  padding: 20,
+                }}
+              >
+                <View
+                  style={{
+                    width: "100%",
+                    padding: 15,
+                    height: "auto",
+                    backgroundColor: Color.textColor,
+                    borderRadius: 20,
+                  }}
+                >
+                  <Image
+                    style={{
+                      width: "100%",
+                      height: Dimensions.get("window").height * 0.25,
+                    }}
+                    contentFit="cover"
+                    source={{
+                      uri: "https://www.crunchyroll.com/imgsrv/display/thumbnail/1200x675/catalog/crunchyroll/1ecde018e863e2aaee31f00a23378c35.jpe",
+                    }}
+                  />
+                </View>
                 <Text
                   style={{
                     color: Color.textColor,
                     fontSize: FontSize.sm,
                     fontWeight: "700",
+                    textAlign: "center",
+                    marginTop: 20,
+                    lineHeight: 20,
+                    paddingHorizontal: 20,
                   }}
                 >
-                  Select Level
+                  {quizQuestions[currentQuestionIndex]?.question}
                 </Text>
-              </View>
-              <View
-                style={{
-                  flexDirection: "row",
-                  gap: 20,
-                  alignItems: "center",
-                }}
-              >
-                <Feather name="bookmark" size={24} color="white" />
-                <Ionicons name="settings-sharp" size={30} color="white" />
-              </View>
-            </View>
-
-            <View style={{
-              padding: 20
-            }}>
-              <View style={{
-                width: "100%",
-                padding: 15,
-                height: "auto",
-                backgroundColor: Color.textColor,
-                borderRadius: 20
-              }}>
-                <Image
+                <FlatList
+                  data={quizQuestions[currentQuestionIndex]?.options}
+                  renderItem={({ item, index }) => (
+                    <TouchableOpacity activeOpacity={0.5} key={index} style={{
+                      flexDirection: "row",
+                      gap: 20,
+                      alignItems: "center",
+                      backgroundColor: Color.card,
+                      padding: 10,
+                      borderRadius: 10,
+                      marginBottom: 15
+                    }}>
+                      <View style={{
+                        borderRadius: 8,
+                        backgroundColor: Color.backgroundColor,
+                        flexDirection: "column",
+                        alignItems: "center",
+                        justifyContent: "center",
+                        width: 30,
+                        height: 30
+                      }}>
+                        <Text
                           style={{
-                            width: "100%",
-                            height: Dimensions.get("window").height*0.25,
+                            color: Color.textColor,
+                            fontSize: FontSize.sm,
+                            fontWeight: "700",
                           }}
-                          contentFit="cover"
-                          source={{
-                            uri: "https://www.crunchyroll.com/imgsrv/display/thumbnail/1200x675/catalog/crunchyroll/1ecde018e863e2aaee31f00a23378c35.jpe"
-                          }}
-                        />
+                        >
+                          {index + 1}
+                        </Text>
+                      </View>
+                      <Text
+                        style={{
+                          color: Color.textColor,
+                          fontSize: FontSize.sm,
+                          fontWeight: "700",
+                        }}
+                      >
+                        {item}
+                      </Text>
+                    </TouchableOpacity>
+                  )}
+                  style={{
+                    marginTop: 30
+                  }}
+                />
               </View>
-            </View>
-          </>
-        )}
-        style={{
-          width: "100%",
-          minHeight: Dimensions.get("window").height,
-          backgroundColor: Color.backgroundColor,
-        }}
-      />
+            </>
+          )}
+          style={{
+            width: "100%",
+            minHeight: Dimensions.get("window").height,
+            backgroundColor: Color.backgroundColor,
+          }}
+        />
+      ) : null}
     </SafeAreaView>
   );
 }
